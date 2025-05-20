ce5e777e64e98520239e5e0eccc66b2cbbb70981
ssssiph <alt.siph@gmail.com>
2025-05-21 02:34:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       projectFrame.className = "project-frame";
       projectFrame.innerHTML = `
         <div class="project-preview">
-          <iframe src="https://test-site-production-f90b.up.railway.app/Projects/${project}/index.html" 
+          <iframe src="https://test-site-production-ab2c.up.railway.app/Projects/${project}/index.html" 
             width="100%" height="200" frameborder="0">
           </iframe>
         </div>

99b63c5af7481acb3b115a600b92daf89e032fd4
ssssiph <alt.siph@gmail.com>
2025-05-21 02:00:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const projectsContainer = document.getElementById("projectsContainer");
-  const projects = ["Project1", "Project2", "Project3"];
+  const projects = ["Project1", "Project2", "Project3"]; // Список папок проектов
 
   for (let project of projects) {
     try {
@@ -11,12 +11,14 @@ document.addEventListener("DOMContentLoaded", async () => {
       let projectFrame = document.createElement("div");
       projectFrame.className = "project-frame";
       projectFrame.innerHTML = `
+        <div class="project-preview" style="background-image: url('Projects/${project}/preview.png');"></div>
         <div class="project-info">
-          <p class="date">Дата создания: ${data.date}</p>
+          <p class="date">${data.date}</p>
           <p class="description">${data.description}</p>
         </div>
         <a href="${data.link}" class="load-button">→</a>
       `;
+
       projectsContainer.appendChild(projectFrame);
     } catch (error) {
       console.error(`Ошибка загрузки проекта ${project}:`, error);
